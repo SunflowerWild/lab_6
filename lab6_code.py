1d7a24f047252308542bee3dc6cad5901f78a198
SunflowerWild <sunflowerwild18@gmail.com>
2023-10-25 12:16:43 -0400

    main code

--- lab6_code.py
+++ lab6_code.py
@@ -1,3 +1,9 @@
+def encode(number):
+    pass
+
+def decode():
+    pass
+
 def main():
     run = True
     while run:
@@ -10,8 +16,10 @@ def main():
         selection = int(input("Please enter an option:"))
         if selection == 1:
             password = int(input("Please enter your password to encode:"))
+            encode(password)
             print('Your password has been encoded and stored!')
         if selection == 2:
+            decode(password)
             print(f'The encoded password is {x}, and the original password is {password}.')
         if selection == 3:
             break
