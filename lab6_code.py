add6f0cc6ec588bae56f4285466d45c719fbf022
Natalie Poche <nataliealena03@gmail.com>
2023-10-25 12:35:51 -0400

    add encoded

--- lab6_code.py
+++ lab6_code.py
@@ -10,14 +10,21 @@ def main():
         selection = int(input("Please enter an option:"))
         if selection == 1:
             password = int(input("Please enter your password to encode:"))
+            new_password = encode(password)
             print('Your password has been encoded and stored!')
         if selection == 2:
             print(f'The encoded password is {x}, and the original password is {password}.')
         if selection == 3:
             break
             run = False
-def encode(): # Natalie Poche
-    pass
+def encode(password): # Natalie Poche
+    encoded = []
+    list = password.split()
+    for i in list:
+        i = i + 3
+        encoded.append(i)
+    encoded = "".join(encoded)
+    return encoded
 
 if __name__ == "__main__":
     main()
