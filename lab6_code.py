1604368a8ebb5d448bb83fecea92ab4071a53801
SunflowerWild <sunflowerwild18@gmail.com>
2023-10-25 12:00:33 -0400

    main code

---
+++ lab6_code.py
@@ -0,0 +1,21 @@
+def main():
+    run = True
+    while run:
+        print('''
+        Menu
+        -------------
+        1. Encode
+        2. Decode
+        3. Quit''')
+        selection = int(input("Please enter an option:"))
+        if selection == 1:
+            password = int(input("Please enter your password to encode:"))
+            print('Your password has been encoded and stored!')
+        if selection == 2:
+            print(f'The encoded password is {x}, and the original password is {password}.')
+        if selection == 3:
+            break
+            run = False
+
+if __name__ == "__main__":
+    main()
